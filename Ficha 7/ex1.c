194e93f0295fe1526387e38ab82185cf9911db12
diogo <diogofgf97@gmail.com>
2024-06-15 01:30:25 +0100

    f7

--- Ficha 7/ex1.c
+++ Ficha 7/ex1.c
@@ -1,3 +1,30 @@
-//
-// Created by diogo on 14-06-2024.
-//
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void preenche(int *a, int tam){ // ultimo loop: a + tam = 8 + 5 = 13; (13, 5)
+    printf("\n%d, %d", *a, tam);
+    for(int i=0; i<tam; i++)
+        a[i] = 5*i;
+}
+int* f1(int *a, int *tam){
+    int *p, i;
+    p = realloc(a, sizeof(int)*(*tam + a[1])); // realocar um novo tamanho de memoria para a data
+    if(p == NULL)                   // 3 + 5 = 8 de espaço realocado
+        return a;
+    a = p;
+    preenche(a + *tam, a[1]);
+    *tam += a[1];
+    return a;
+}
+int main(){
+    int *v = NULL, total = 0;
+    v = malloc(sizeof(int) * 3); // alocar 3ints the espaço
+    if(v == NULL)
+        return 0;
+    total = 3;
+    preenche(v, total); // 5 10 15
+    v = f1(v, &total);
+    free(v);
+    return 0;
+}
--- Ficha 7/ex2.c
+++ Ficha 7/ex2.c
@@ -1,3 +1,102 @@
-//
-// Created by diogo on 14-06-2024.
-//
+#include <stdio.h>
+#include <stdbool.h>
+#include <malloc.h>
+#include <string.h>
+
+typedef struct {
+    char *nome;
+    int numero;
+} Agenda;
+
+bool addToAgenda(Agenda **agenda, int *tam, char*userNome, int userContacto) {
+    for(int i=0; i<*tam; i++) {
+        if (strcmp(userNome, (*agenda)[i].nome) == 0) {
+            printf("Nome já em utilizacao!\n");
+            return false;
+        }
+    }
+
+    *agenda = realloc(*agenda, sizeof(Agenda) * (*tam + 1));
+    (*agenda)[*tam].nome = strdup(userNome);
+    (*agenda)[*tam].numero = userContacto;
+    (*tam)++;
+    return true;
+}
+
+void printList(Agenda *agenda, int *tam) {
+    for (int i=0; i<*tam; i++) {
+        printf("Nome: %s, Contacto: %d\n", agenda[i].nome, agenda[i].numero);
+    }
+}
+
+int lookForPhoneNumberOfContact(Agenda *agenda, int tam, char *contact) {
+    for (int i=0; i<tam; i++) {
+        if (strcmp(agenda[i].nome, contact) == 0)
+            return agenda[i].numero;
+    }
+    return -1;
+}
+
+int updateContact(Agenda **agenda, int tam, char *contact, int newNr) {
+    for (int i=0; i<tam; i++) {
+        if (strcmp((*agenda)[i].nome, contact) == 0) {
+            (*agenda)[i].numero = newNr;
+            printf("Updated %s's number to: %d", (*agenda)[i].nome, newNr);
+            return 1;
+        }
+    }
+    return 0;
+}
+
+Agenda* deleteContact(Agenda **agenda, int *tam, char *personToDelete) {
+    for (int i=0; i<*tam; i++) {
+        if (strcmp((*agenda)[i].nome, personToDelete) == 0) {
+            free((*agenda)[i].nome);
+
+            for (int j=i; j<(*tam-1); j++) {
+                (*agenda)[j] = (*agenda)[j+1];
+            }
+
+            (*tam)--;
+
+            Agenda *temp = realloc(*agenda, sizeof(Agenda) * (*tam));
+            if (temp == NULL && *tam > 0) {
+                printf("Erro de realocação de memória!\n");
+                return NULL;
+            }
+
+            *agenda = temp;
+            return *agenda;
+
+        }
+    }
+    return NULL;
+}
+
+
+int main() {
+    Agenda *agenda = NULL;
+    int tamAgenda = 0;
+
+    addToAgenda(&agenda, &tamAgenda, "to", 991);
+    addToAgenda(&agenda, &tamAgenda, "maria", 222);
+    addToAgenda(&agenda, &tamAgenda, "lena", 342);
+
+    printList(agenda, &tamAgenda);
+
+    int res = lookForPhoneNumberOfContact(agenda, tamAgenda, "maria");
+    if (res != -1)
+        printf("\nNumero de '%s': %d\n", "maria", res);
+
+    updateContact(&agenda, tamAgenda, "to", 1337);
+
+    Agenda *delRes = deleteContact(&agenda, &tamAgenda, "lena");
+    if (delRes != NULL)
+        printf("\nUser deleted!\n");
+
+
+
+
+    free(agenda);
+
+}
